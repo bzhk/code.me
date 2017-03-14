4e77dd7a4d4709bd5ba3816a2b1d4f3d7036fce9
baro <baro@wacek>
2017-03-14 20:08:16 +0100

    less signout and nav

--- code.me/routes/views/signout.js
+++ code.me/routes/views/signout.js
@@ -2,12 +2,8 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 
-	var view = new keystone.View(req, res),
-		locals = res.locals;
-
-	locals.section = 'session';
-
 	keystone.session.signout(req, res, function() {
+		req.flash('success','Zostałeś poprawnie wylogowany.');
 		res.redirect('/');
 	});
 
